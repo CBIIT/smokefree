34a9ee54997d7bcdafd38710f9ab442722cd9bdd
Douglas Groene <douglas.groene@icfi.com>
2017-09-18 15:28:20 -0400

    DEG docksal project wrapper

---
+++ drush/aliases/local.aliases.drushrc.php
@@ -0,0 +1,9 @@
+<?php
+// local alias
+$aliases['local'] = array(
+'root' => '/var/www/smokefree/',
+'path-aliases' => array(
+'%dump-dir' => '/var/www/database_dumps/',
+'%files' => '/var/www/smokefree/sites/default/files'
+)
+);
