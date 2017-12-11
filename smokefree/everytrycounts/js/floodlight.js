e50b862efa5598a560a255dc0f5a0e9793b04525
shashank-naik <shashank.naik@icf.com>
2017-12-11 15:21:03 -0500

    SF-651: Changing functionality to be a pure JS one

--- smokefree/everytrycounts/js/floodlight.js
+++ smokefree/everytrycounts/js/floodlight.js
@@ -1,18 +1,7 @@
-
-jQuery(document).ready(function () {
-    jQuery('.appDownloads .ios img').on( "click", function() {
-        console.log('Clicked iOS Button');
-        makeFloodlightTagCall('DC-4345482/CTP/posev006+standard');
+function makeFloodlightTagCall(value) {
+    console.log(value);
+    gtag('event', 'conversion', {
+        'allow_custom_scripts': true,
+        'send_to': value
     });
-    jQuery('.appDownloads .android img').on( "click", function() {
-        console.log('Clicked Android Button');
-        makeFloodlightTagCall('DC-4345482/CTP/posev007+standard');
-    });
-
-    function makeFloodlightTagCall(value) {
-        gtag('event', 'conversion', {
-            'allow_custom_scripts': true,
-            'send_to': value
-        });
-    }
-});
+}
