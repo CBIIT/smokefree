68592f17f5637835f7f1f50dae3fc2dfab3b5580
shashank-naik <shashank.naik@icf.com>
2018-04-06 17:31:07 -0400

    Homepage: Recompiling SASS files

--- smokefree/sites/all/themes/smokefreegov/gulpfile.js
+++ smokefree/sites/all/themes/smokefreegov/gulpfile.js
@@ -4,7 +4,18 @@
 var gulp = require('gulp');
 // var less = require('gulp-less');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+var cache = require('gulp-cache');
+var runSequence = require('run-sequence');
+var minify = require('gulp-minify');
+var cleanCss = require('gulp-clean-css');
+var cssnano = require('gulp-cssnano');
+var cached = require('gulp-cached');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 var sourcemaps = require('gulp-sourcemaps');
+var imageminMozjpeg = require('imagemin-mozjpeg');
 // var tinify = require('gulp-tinify');
 // var imageminJpegRecompress = require('imagemin-jpeg-recompress');
 // var notify = require('gulp-notify');
@@ -12,8 +23,19 @@ var sourcemaps = require('gulp-sourcemaps');
 
 /************Working Tasks************/
 
+// gulp.task('less', function() {
+// 	return gulp.src('src/less/style.less')
+// 		.pipe(less())
+// 		//.pipe(cleanCss())
+//         .pipe(cssnano())
+// 		.pipe(gulp.dest('dist/css'))
+// });
 gulp.task('sass', function() {
     return gulp.src('src/scss/style.scss')
+        //.pipe(sass().on('error', sass.logError))
+        //.pipe(cleanCss())
+        //.pipe(cssnano())
+        //.pipe(gulp.dest('css'))
         .pipe(sourcemaps.init())
         .pipe(sass({
             noCache: true,
@@ -27,14 +49,39 @@ gulp.task('sass', function() {
         })
         .pipe(sourcemaps.write('src/maps'))
         .pipe(gulp.dest('css'))
-
+        // .pipe(notify({
+        //     title: "SASS Compiled",
+        //     message: "All SASS files have been recompiled to CSS",
+        //     onLast: true
+        // }));
 });
-
+gulp.task('js', function() {
+	return gulp.src('src/js/**/*.js')
+		//.pipe(minify())
+        .pipe(cached('script'))
+        .pipe(uglify())
+		.pipe(gulp.dest('js'))
+});
+gulp.task('images', function() {
+	return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        // .pipe(cache(imagemin()))
+        //.pipe(imagemin({optimizationLevel: 1, progressive: true, interlaced: true, multipass: true}))
+        .pipe(imagemin({
+            use:[imageminMozjpeg({
+                quality: 10
+            })]
+        }))
+		.pipe(gulp.dest('images'))
+});
+gulp.task('fonts', function() {
+	return gulp.src('src/fonts/**/*')
+		.pipe(gulp.dest('fonts'))
+});
 
 /************Cleaning Tasks************/
 
 gulp.task('clean-up', function() {
-    //return del.sync('dist')
+	//return del.sync('dist')
     return del([
         //'dist/css/**/*', //who put images folder under css folder? Please move it under smokefree theme root folder
         'css/style.css',
@@ -47,5 +94,5 @@ gulp.task('clean-up', function() {
 
 // Default Task
 gulp.task('default', function(callback) {
-    runSequence('clean-up', ['sass', 'js', 'images'], callback)
+	runSequence('clean-up', ['sass', 'js', 'images'], callback)
 });
