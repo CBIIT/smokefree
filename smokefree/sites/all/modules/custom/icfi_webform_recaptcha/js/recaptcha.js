929f370da7e50f4d0b281cb935a88a40ec13f251
shashank-naik <shashank.naik@icf.com>
2018-08-07 12:06:15 -0400

    cdc : removing recaptcha testing code

--- smokefree/sites/all/modules/custom/icfi_webform_recaptcha/js/recaptcha.js
+++ smokefree/sites/all/modules/custom/icfi_webform_recaptcha/js/recaptcha.js
@@ -6,8 +6,3 @@ jQuery(document).ready(function() {
     var captcha = jQuery('.captcha');
     jQuery(".node-webform .webform-submit.button-primary.btn.form-submit").before(captcha);
 });
-
-if (window.innerWidth < 768 && (/iPhone|iPod/.test(navigator.userAgent) && !window.MSStream)) {
-    var destElementOffset = jQuery('.g-recaptcha').position().top - window.innerWidth;
-    jQuery('html, body').animate({ scrollTop: destElementOffset }, 0);
-}
