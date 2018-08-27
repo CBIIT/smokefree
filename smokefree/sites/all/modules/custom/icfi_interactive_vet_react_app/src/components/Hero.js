f991ca3db6135428e95352f62b3e695b0b630dc0
dboone <derrick.boone@icf.com>
2018-08-27 13:31:52 -0400

    SOW-1537 -Committing Updated src files for review

--- smokefree/sites/all/modules/custom/icfi_interactive_vet_react_app/src/components/Hero.js
+++ smokefree/sites/all/modules/custom/icfi_interactive_vet_react_app/src/components/Hero.js
@@ -12,10 +12,10 @@ const Hero = () => {
                 <div className="col-md-6">
                     <p>Nicotine replacement therapy (NRT) is safe, effective, and the most commonly used family of quit-tobacco medications. It reduces withdrawal feelings by providing nicotine—but none of the dangerous chemicals that you’re exposed to when you use tobacco. NRT delivers far less nicotine than cigarettes or smokeless tobacco products do, and much more slowly. This makes it less addictive. Most people find it easy to get off nicotine medicines after a few months.</p>
                     
-                    <p>Nicotine medicines are proven to increase your chances of quitting, but they won’t completely take away the urge to smoke. For the best results, follow use directions carefully. Use enough and use it for the recommended time. Combining NRT with other strategies can improve your chances of quitting and staying quit.</p>
+                    <p>Nicotine medicines are proven to increase your chances of quitting, but they won’t completely take away the urge to smoke. For best results, carefully follow the directions for using NRT. Use a high enough dose, and use the medicine for the recommended time (2–4 months). Combining NRT with counseling can improve your chances of quitting and staying quit.</p>
                 </div> 
                 <div className="col-md-6">
-                    <img src="/images/quit-with-nrt.jpg" className="img-responsive" alt="Quitting with NRT is safe and effective." />
+                    <img src="/sites/all/modules/custom/icfi_interactive_vet_react_app/build/images/quit-with-nrt.jpg" className="img-responsive" alt="Quitting with NRT is safe and effective." />
                 </div> 
             </section> 
         </div>
