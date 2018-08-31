85af882b79b3a5adb45bd3288995e0d4a0e0e13d
dboone <derrick.boone@icf.com>
2018-08-31 14:16:52 -0400

    Merge remote-tracking branch 'origin/SOW-1537' into SOW-1537 updated calculator changes.

--- smokefree/sites/all/modules/custom/icfi_interactive_vet_react_app/invet-dev/src/components/Calculator.js
+++ smokefree/sites/all/modules/custom/icfi_interactive_vet_react_app/invet-dev/src/components/Calculator.js
@@ -7,42 +7,45 @@ class Calculator extends Component {
         super();
         this.state = {
             showCigarettes: true,
+            showChew: false,
             cigPanel: 'active',
             chewPanel: null
         }
-    } 
-    
-  render() {
-    const cigPane = () => {
-        this.setState({
-            showCigarettes: true,
-            cigPanel: 'active',
-            chewPanel: null
-        });
     }
-    const chewPane = () => {
-        this.setState({
-            showCigarettes: false, 
-            cigPanel: null, 
-            chewPanel: 'active'
-        });
-    }
-    return (
-        <div className="row">
+
+    render() {
+        const cigPane = () => {
+            this.setState({
+                showCigarettes: true,
+                showChew: false,
+                cigPanel: 'active',
+                chewPanel: null
+            });
+        }
+        const chewPane = () => {
+            this.setState({
+                showCigarettes: false,
+                showChew: true,
+                cigPanel: null,
+                chewPanel: 'active'
+            });
+        }
+        return (
+            <div className="row">
                 <div className="col-md-12">
                     <div className="dosage">
                         <p></p>
-                        <ul className="nav vet-nav-tabs" role="tablist">
+                        <ul className="nav nav-tabs" role="tablist">
                             <li className="nonactive"><b>Do You…</b></li>
                             <li role="presentation" className={this.state.cigPanel}><a onClick={cigPane} style={{cursor:'pointer'}} aria-controls="cigarettes" role="tab" data-toggle="tab"><span className="tab-text">Smoke Cigarettes?</span></a></li>
                             <li role="presentation" className={this.state.chewPanel}><a onClick={chewPane} style={{cursor:'pointer'}} aria-controls="smokeless" role="tab" data-toggle="tab"><span className="tab-text">Dip or Chew Tobacco?</span></a></li>
                         </ul>
-                        <CigarettePanel showCigarettes={this.state.showCigarettes} />
+                        <CigarettePanel showChew={this.state.showChew} showCigarettes={this.state.showCigarettes} />
                     </div>
                 </div>
             </div>
-    )
-  }
+        )
+    }
 }
 
 export default Calculator;
--- smokefree/sites/all/modules/custom/icfi_interactive_vet_react_app/invet-dev/src/components/CigarettePanel.js
+++ smokefree/sites/all/modules/custom/icfi_interactive_vet_react_app/invet-dev/src/components/CigarettePanel.js
@@ -15,129 +15,126 @@ class CigarettePanel extends Component {
             }
         }
     }
-  render() {
-    const { showCigarettes } = this.props;
+    render() {
+        const { showCigarettes, showChew } = this.props;
 
-    const calculatorData = {
-        cigarettes: [
-            {
-                dosage: '10 or fewer',
-                gum: '2mg',
-                patch: '14mg',
-                key: 1
-            },
-            {
-                dosage: '11-20',
-                gum: '2mg',
-                patch: '21mg',
-                key: 2
-            },
-            {
-                dosage: '21 or more',
-                gum: '4mg',
-                patch: '21mg',
-                key: 3
-            }
-        ]
-    }
-    
-    const cigaretteChangeHandler = () => {
-        if(this.state.data.key <= 2) {
-            this.setState({
-                data: calculatorData.cigarettes[0 + this.state.data.key]
-            })
-        } else {
-            this.setState({
-                data: {
+        const calculatorData = {
+            cigarettes: [
+                {
                     dosage: '10 or fewer',
                     gum: '2mg',
                     patch: '14mg',
                     key: 1
+                },
+                {
+                    dosage: '11-20',
+                    gum: '2mg',
+                    patch: '21mg',
+                    key: 2
+                },
+                {
+                    dosage: '21 or more',
+                    gum: '4mg',
+                    patch: '21mg',
+                    key: 3
                 }
-            })
+            ]
         }
-    }
-    
-    const cigaretteChangeHandlerDown = () => {
-        if(this.state.data.key === 1) {
-            this.setState({
-                data: calculatorData.cigarettes[2]
-            })
-        } else if(this.state.data.key === 2) {
-            this.setState({
-                data: calculatorData.cigarettes[0]
-            })
-        } else if(this.state.data.key === 3) {
-            this.setState({
-                data: calculatorData.cigarettes[1]
-            })
-        }
-    }  
 
-    return (
-        <div className="tab-content">
-            {showCigarettes ? (
-                <div role="tabpanel" className="tab-pane active" id="cigarettes">
-                <div className="row">
-                    <div className="col-md-8 col-md-offset-3">
-                        <h3>Estimate Your Nicotine Dosage</h3>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-3 col-md-offset-3 col-sm-6 col-sm-offset-3">
-                        <p className="larger">How many cigarettes do you smoke per day?</p>
-                        <div className="spacer-10"></div>
-                        <div className="row">
-                            <div className="col-md-3 col-xs-3">
-                                <button onClick={cigaretteChangeHandlerDown} className="btn-invet btn-default btn-minus">
-                                    <span className="glyphicon glyphicon-minus"></span>
-                                    <span className="sr-only">Minus</span>
-                                </button>
-                            </div>
-                            <div className="col-md-6 col-xs-6">
-                                <div className="cigarette-amount">{this.state.data.dosage}</div>
-                            </div>
-                            <div className="col-md-3 col-xs-3">
-                                <button onClick={cigaretteChangeHandler} className="btn-invet btn-default btn-plus pull-right">
-                                    <span className="glyphicon glyphicon-plus"></span>
-                                    <span className="sr-only">Plus</span>
-                                </button>
-                            </div>
+        const cigaretteChangeHandler = () => {
+            if(this.state.data.key <= 2) {
+                this.setState({
+                    data: calculatorData.cigarettes[0 + this.state.data.key]
+                })
+            } else {
+                this.setState({
+                    data: {
+                        dosage: '10 or fewer',
+                        gum: '2mg',
+                        patch: '14mg',
+                        key: 1
+                    }
+                })
+            }
+        }
+
+        const cigaretteChangeHandlerDown = () => {
+            if(this.state.data.key === 1) {
+                this.setState({
+                    data: calculatorData.cigarettes[2]
+                })
+            } else if(this.state.data.key === 2) {
+                this.setState({
+                    data: calculatorData.cigarettes[0]
+                })
+            } else if(this.state.data.key === 3) {
+                this.setState({
+                    data: calculatorData.cigarettes[1]
+                })
+            }
+        }
+
+        return (
+            <div className="tab-content">
+                <div role="tabpanel" className="tab-pane active" id="cigarettes" style={{display: showCigarettes ? 'block' : 'none' }}>
+                    <div className="row">
+                        <div className="col-md-8 col-md-offset-3">
+                            <h3>Estimate Your Nicotine Dosage</h3>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-lg-offset-2 col-md-5 col-md-offset-1 col-sm-12">
-                        <div className="box">
-                            <div className="cigarette-solution">
-                                <h4>TRY THESE NRTs<span className="red">*</span></h4>
-                                <div className="row">
-                                    <div className="col-lg-4 col-md-5 col-sm-4">
-                                        <ReactCSSTransitionGroup component="div" className="solution-wrapper" transitionName="fade" transitionEnterTimeout={500} transitionLeave={false}>
-                                            <div key={this.state.data.gum} className="circle gum-amount">{this.state.data.gum}</div>
-                                            <div className="solution-type"><b>Gum or Lozenge</b></div>
-                                        </ReactCSSTransitionGroup>
-                                    </div> 
-                                    <div className="col-lg-4 col-md-2 col-sm-4">
-                                        <div className="and"></div>
-                                    </div>   
-                                    <div className="col-lg-4 col-md-5 col-sm-4">
-                                        <ReactCSSTransitionGroup component="div" className="solution-wrapper" transitionName="fade" transitionEnterTimeout={500} transitionLeave={false}>
-                                            <div key={this.state.data.patch} className="circle patch-amount">{this.state.data.patch}</div>
-                                            <div className="solution-type"><b>Patch</b></div>
-                                        </ReactCSSTransitionGroup>
-                                    </div>
+                    <div className="row">
+                        <div className="col-md-3 col-md-offset-3 col-sm-6 col-sm-offset-3">
+                            <p className="larger">How many cigarettes do you smoke per day?</p>
+                            <div className="spacer-10"></div>
+                            <div className="row">
+                                <div className="col-md-3 col-xs-3">
+                                    <button onClick={cigaretteChangeHandlerDown} className="btn-invet btn-default btn-minus">
+                                        <span className="glyphicon glyphicon-minus"></span>
+                                        <span className="sr-only">Minus</span>
+                                    </button>
+                                </div>
+                                <div className="col-md-6 col-xs-6">
+                                    <div className="cigarette-amount">{this.state.data.dosage}</div>
+                                </div>
+                                <div className="col-md-3 col-xs-3">
+                                    <button onClick={cigaretteChangeHandler} className="btn-invet btn-default btn-plus pull-right">
+                                        <span className="glyphicon glyphicon-plus"></span>
+                                        <span className="sr-only">Plus</span>
+                                    </button>
                                 </div>
                             </div>
                         </div>
-                        <p className="small">*See NRT package inserts for complete dosing recommendations and talk to your health care provider.</p>
+                        <div className="col-lg-4 col-lg-offset-2 col-md-5 col-md-offset-1 col-sm-12">
+                            <div className="box">
+                                <div className="cigarette-solution">
+                                    <h4>TRY THESE NRTs<span className="red">*</span></h4>
+                                    <div className="row">
+                                        <div className="col-lg-4 col-md-5 col-sm-4">
+                                            <ReactCSSTransitionGroup component="div" className="solution-wrapper" transitionName="fade" transitionEnterTimeout={500} transitionLeave={false}>
+                                                <div key={this.state.data.key} className="circle gum-amount">{this.state.data.gum}</div>
+                                                <div className="solution-type"><b>Gum or Lozenge</b></div>
+                                            </ReactCSSTransitionGroup>
+                                        </div>
+                                        <div className="col-lg-4 col-md-2 col-sm-4">
+                                            <div className="and"></div>
+                                        </div>
+                                        <div className="col-lg-4 col-md-5 col-sm-4">
+                                            <ReactCSSTransitionGroup component="div" className="solution-wrapper" transitionName="fade" transitionEnterTimeout={500} transitionLeave={false}>
+                                                <div key={this.state.data.key} className="circle patch-amount">{this.state.data.patch}</div>
+                                                <div className="solution-type"><b>Patch</b></div>
+                                            </ReactCSSTransitionGroup>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="small">*See NRT package inserts for complete dosing recommendations and talk to your health care provider.</p>
+                        </div>
                     </div>
                 </div>
-                </div>
-            ) : (
-               <ChewPanel /> 
-            )}
-        </div>
-    )
-  }
+                <ChewPanel showChew={showChew} />
+            </div>
+        )
+    }
 }
 
 export default CigarettePanel;
