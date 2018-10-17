6acccd9c6fa74d964f93a3ca0a3351ddde86c35a
NLM-ICF <kpongyu@gmail.com>
2018-10-17 16:47:13 -0400

    created the Instagram card, add animation on the try-an-app section

--- smokefree/sites/all/modules/custom/icf_lgbt_app/src/appshell/Instagram/Instagram.js
+++ smokefree/sites/all/modules/custom/icf_lgbt_app/src/appshell/Instagram/Instagram.js
@@ -6,8 +6,14 @@ import 'react-bootstrap/dist/react-bootstrap';
 class Instagram extends Component {
     render() {
       return (
-        <div>
-            <img src="/sites/all/modules/custom/icf_lgbt_app/src/assets/instagram.png" className="img-responsive" alt=""/>
+        <div className="instagram-section">
+           <h2>SmokefreeUS Instagram</h2>
+           <p>Together we can quit smoking! Join us on <a href="https://www.instagram.com/smokefreeus/?hl=en" target="_blank">Instagram</a> for motivational images, helpful tips, and inspiration.</p>
+           
+           <a href="https://www.instagram.com/smokefreeus/?hl=en" className="button"><span className="buttonText">Go To Instagram</span></a>
+
+            <img src="/sites/all/modules/custom/icf_lgbt_app/src/assets/instagram-photo.png" className="img-responsive" alt="instagram cover"/>
+        
         </div>
        
       )
