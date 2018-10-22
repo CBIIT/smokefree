0e655cb951e9f8b4cf7ec3cd54cfe67ba5f0a8bc
NLM-ICF <kpongyu@gmail.com>
2018-10-21 21:36:57 -0400

    layout the "Strong Enough to quit", and "smokefreetxt" card

--- smokefree/sites/all/modules/custom/icf_lgbt_app/src/appshell/SmokefreeTXT/SmokefreeTXT.js
+++ smokefree/sites/all/modules/custom/icf_lgbt_app/src/appshell/SmokefreeTXT/SmokefreeTXT.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import './SmokefreeTXT.css';
 import 'react-bootstrap/dist/react-bootstrap';
-
-
+import {Row, Col} from 'react-bootstrap/lib';
+import FlexView from 'react-flexview';
 class SmokefreeTXT extends Component {
     render() {
       return (
-        <div>
-            <img src="/sites/all/modules/custom/icf_lgbt_app/src/assets/smokefreetxt.png" className="img-responsive" alt=""/>
+        <div className="txt">
+        <div className="brush">
+        <FlexView vAlignContent="center" width="100%">
+        <Row className="txtInputRow">
+          <Col xs={12} md={8} lg={9}>
+          <input type="text" className="txtInput" placeholder="Phone Number"></input>
+          </Col>
+          <Col xs={12} md={4} lg={3}>
+          <a className="button" href="#" target="_blank"><span className="buttonText">Start Now</span></a>
+          </Col>
+          <Col xs={12}>
+          <p className="unsubscribe">To unsubscribe text STOP to 47848</p>
+          </Col>
+        </Row>
+        </FlexView>
+        </div>
+          <Row >
+            <Col xs={12} md={12} lg={6} sm={12}>
+            <h2>SmokefreeTXT</h2>
+            <p>Get daily text messages with encouragement, advice, and tips to help you quit smoking.</p>
+            </Col>
+          </Row>
+          <img src="/sites/all/modules/custom/icf_lgbt_app/src/assets/text-bubbles.png" className="txtimage" alt="text bubbles" />
         </div>
        
       )
--- smokefree/sites/all/modules/custom/icf_lgbt_app/src/appshell/StrongEnough/StrongEnough.js
+++ smokefree/sites/all/modules/custom/icf_lgbt_app/src/appshell/StrongEnough/StrongEnough.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './StrongEnough.css';
 import 'react-bootstrap/dist/react-bootstrap';
-
+import {Row, Col} from 'react-bootstrap/lib';
 
 class StrongEnough extends Component {
     render() {
       return (
-        <div>
-            <img src="/sites/all/modules/custom/icf_lgbt_app/src/assets/strong-enough.png" className="img-responsive" alt=""/>
+        <div className="strongEnough">
+         <img src="/sites/all/modules/custom/icf_lgbt_app/src/assets/strong-enough-brush.png" className="brush" alt="background brush"  />
+          <Row>
+            <Col xs={12} md={8} lg={6} sm={12}>
+            <h2>You are strong enough to quit</h2>
+            <p>LGBT people face adversity and overcome it every day. 
+              You can use that same strength to quit smoking. <strong>We’ve got tools to help you</strong>:</p>
+            <ul>
+              <li>Set a quit date and create your personalized <a src="#" target="_blank">quit plan</a>.</li>
+              <li>Talk to a doctor about <a src="#" target="_blank">medications</a> and other ways to help you quit.</li>
+              <li>Call a quitline. Try <a src="#" target="_blank">1-800-QUIT-NOW</a>(1-800-784-8669) or <a src="#" target="_blank">1-877-44U-QUIT</a> (1-877-448-7848).</li>
+              <li>Find an LGBT–friendly healthcare provider through the <a src="#" target="_blank">Gay and Lesbian Medical Association</a>.</li>
+            </ul>
+            </Col>
+          </Row>
+         
         </div>
        
       )
