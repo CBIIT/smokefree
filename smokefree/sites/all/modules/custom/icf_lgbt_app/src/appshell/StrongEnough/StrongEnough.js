89a53b35fe0acdef49484d1d5cbb884262f328ed
NLM-ICF <kpongyu@gmail.com>
2018-10-29 11:09:19 -0400

    add links to the "Strong Enough to quit" card

--- smokefree/sites/all/modules/custom/icf_lgbt_app/src/appshell/StrongEnough/StrongEnough.js
+++ smokefree/sites/all/modules/custom/icf_lgbt_app/src/appshell/StrongEnough/StrongEnough.js
@@ -14,10 +14,10 @@ class StrongEnough extends Component {
             <p>LGBT people face adversity and overcome it every day. 
               You can use that same strength to quit smoking. <strong>We’ve got tools to help you</strong>:</p>
             <ul>
-              <li>Set a quit date and create your personalized <a src="#" target="_blank">quit plan</a>.</li>
-              <li>Talk to a doctor about <a src="#" target="_blank">medications</a> and other ways to help you quit.</li>
-              <li>Call a quitline. Try <a src="#" target="_blank">1-800-QUIT-NOW</a>(1-800-784-8669) or <a src="#" target="_blank">1-877-44U-QUIT</a> (1-877-448-7848).</li>
-              <li>Find an LGBT–friendly healthcare provider through the <a src="#" target="_blank">Gay and Lesbian Medical Association</a>.</li>
+              <li>Set a quit date and create your personalized <a href="https://smokefree.gov/build-your-quit-plan" target="_blank">quit plan</a>.</li>
+              <li>Talk to a doctor about <a href="https://smokefree.gov/tools-tips/how-to-quit/medications-can-help-you-quit" target="_blank">medications</a> and other ways to help you quit.</li>
+              <li>Call a quitline. Try <a href="tel:18007848669" target="_blank">1-800-QUIT-NOW</a>(1-800-784-8669) or <a href="tel:18774487848" target="_blank">1-877-44U-QUIT</a> (1-877-448-7848).</li>
+              <li>Find an LGBT–friendly healthcare provider through the <a href="http://www.glma.org/index.cfm?fuseaction=Page.viewPage&pageId=939&grandparentID=534&parentID=938&nodeID=1" target="_blank">Gay and Lesbian Medical Association</a>.</li>
             </ul>
             </Col>
           </Row>
